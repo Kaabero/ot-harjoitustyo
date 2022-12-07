dbcedaed628cf39d6641deb0159b0cadb90a66c3
Kaabero <117173101+Kaabero@users.noreply.github.com>
2022-12-07 12:38:10 +0200

    Update tasks.py

--- tasks.py
+++ tasks.py
@@ -6,11 +6,11 @@ def start(ctx):
 
 @task
 def test(ctx):
-    ctx.run("pytest -s src", pty=True)
+    ctx.run("pytest src", pty=True)
 
 @task
 def coverage(ctx):
-    ctx.run("coverage run --branch -m pytest -s src", pty=True)
+    ctx.run("coverage run --branch -m pytest src", pty=True)
 
 @task(coverage)
 def coverage_report(ctx):
